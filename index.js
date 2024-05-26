c8e734f7aba45769794b4a9f26deac111b4b202f
SarahFrainer <sbfrainer@gmail.com>
2024-05-26 15:14:00 -0300

    ex1

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log ("teste");
+
+function filtrarPares(numeros) {
+    let numerosPares = numeros.filter((numero) => {
+        return numero % 2 === 0;
+    })
+    return numerosPares;
+    }
+    
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(filtrarPares(numeros)); 
