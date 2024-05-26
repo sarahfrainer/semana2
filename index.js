b8f276f281008d7b3e427ddc97a7a9a7417ff1b9
SarahFrainer <sbfrainer@gmail.com>
2024-05-26 15:49:11 -0300

    ex-2

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-console.log ("teste");
-
 function filtrarPares(numeros) {
     let numerosPares = numeros.filter((numero) => {
         return numero % 2 === 0;
@@ -9,3 +7,21 @@ function filtrarPares(numeros) {
     
 let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(filtrarPares(numeros)); 
+
+
+function filtrarAdultos(pessoas) {
+    let maioresDeIdade = pessoas.filter((pessoa) => {
+        if (pessoa.idade > 18) {
+            return pessoa;
+        }
+    }).map((pessoa) => pessoa.nome);
+    return maioresDeIdade;
+}
+
+    let pessoas = [
+    { nome: 'Alice', idade: 17 },
+    { nome: 'Bob', idade: 22 },
+    { nome: 'Charlie', idade: 16 },
+    { nome: 'David', idade: 19 }
+    ];
+    console.log(filtrarAdultos(pessoas));
